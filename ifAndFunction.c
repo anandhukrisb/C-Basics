0a63f3e898049dbbfec3e3db45db6de97085b296
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-07-06 12:55:46 +0530

    half way through mastering if and functions

--- ifAndFunction.c
+++ ifAndFunction.c
@@ -1,10 +1,80 @@
 #include <stdio.h>
 
+// void greet() {
+//     printf("Welcome to C programming!");
+// }
+
+// void printTable() {
+//     for(int i = 1; i <= 10; i++) {
+//         printf("%d x 5 = %d\n", i, i * 5);
+//     }
+// }
+
+// void displayName() {
+//     printf("My name is Anandhu\n");
+// }
+
+// void displayAge() {
+//     printf("Im 21 years old\n");
+// }
+
+// void displayCollege() {
+//     printf("My college is Rajagiri College of Social Sciences\n");
+// }
+
+// void printSquare(int n) {
+//     printf("Square of %d is %d", n, n * n);
+// }
+
+// void calculateArea(float r) {
+//     const float PI = 3.14;
+//     printf("Area of circle with radius %.3f is %.3f", r, PI * r * r);
+// }
+
+// void checkEvenOdd(int number) {
+//     number % 2 == 0 ? printf("Even number") : printf("Odd number");
+// }
+
 int main() {
 
     // BASICS
 
     // Q) Write a function void greet() that prints “Welcome to C programming!”
+    
+    //  greet();
+
+    // Q) Create a function void printTable() that prints the multiplication table of 5.
+
+    // printTable();
+
+    // Q) Write a main() function that calls 3 different void functions to print your name, age, and college.
+
+    // displayName();
+    // displayAge();
+    // displayCollege();
+
+    // Level 2
+
+    // Q) Create a function void printSquare(int n) that prints the square of the given number.
+
+    // int n = 3;
+
+    // printSquare(n);
+
+    // Q) Write a function void calculateArea(float radius) that prints area of a circle.
+
+    // float radius = 2;
+
+    // calculateArea(radius);
+
+    // Q) Create a void checkEvenOdd(int number) function that prints whether the number is even or odd (use if inside function).
+
+    // int number = 3;
+
+    // checkEvenOdd(number);
+
+    // Q) Create a function int getFixedNumber() that always returns 10. Print it in main().
+
     
 
     return 0;
