53d1db5f6a10d4f48990b92a947a9c0caca8f82f
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-07-10 00:05:12 +0530

    Starting loops.c

---
+++ loops.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    
+    // BASICS
+
+    // Q) Print numbers from 1 to 10 using a while loop.
+    
+    // int n = 1;
+
+    // while(n <= 10) {
+    //     printf("%d\n", n);
+    //     n++;
+    // }
+
+    // Q) Print all even numbers between 1 and 100 using while.
+
+    // int n = 1;
+
+    // while(n <= 100) {
+    //     n % 2 == 0 ? printf("%d\n", n) : printf("");
+    //     n++;
+    // }
+
+    // Q) Take a number n from the user, and print its multiplication table using while.
+
+    // int n, i = 1;
+
+    // printf("Enter the number: ");
+    // scanf("%d", &n);
+
+    // while(i <= 10) {
+    //     printf("%d x %d = %d\n", i, n, i * n);
+    //     i++;
+    // }
+
+    return 0;
+}
