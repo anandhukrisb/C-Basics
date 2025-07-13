0cb4c58a7e9790aceca22eb0c87110eb48a6c94e
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-07-13 13:10:49 +0530

    loop.c completed

--- loops.c
+++ loops.c
@@ -137,12 +137,125 @@ int main() {
         // * * *
         // * * * *
     
-    for(int i = 1; i <= 4; i++) {
-        for(int j = i; j <= i + 1; j++) {
-            printf("* ");
-        }
-        printf("\n");
-    }
+    // for(int i = 1; i <= 4; i++) {
+    //     for(int j = 1; j <= i; j++) {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // Q) Print a pyramid of numbers:
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+
+    // for(int i = 1; i <= 4; i++) {
+    //     for(int j = 1; j <= i; j++) {
+    //         printf("%d ", j);
+    //     }
+    //     printf("\n");
+    // }
+
+    // Q) Print a reverse triangle:
+        // * * * *
+        // * * *
+        // * *
+        // *
+
+    // for(int i = 1; i <= 4; i++) {
+    //     for(int j = 4; j > i - 1; j--) {
+    //         printf("* ");
+    //     }
+    //     printf("\n");   
+    // }
+
+    // Q) Print multiplication tables from 1 to 10 using nested for loops.
+
+    // for(int i = 1; i <= 10; i++) {
+    //     for(int j = 1; j <= 10; j++) {
+    //         printf("%d x %d = %d\n", i, j, i*j);
+    //     }
+    //     printf("\n");
+    // }
+
+    // LEVEL 4: Expert – Logic & Conditions in Loops
+
+    // Q) Take a number and print all its factors using a loop.
+
+    // int n = 12;
+
+    // printf("Factors of this number are: \n");
+    // for(int i = 1; i <= n; i++) {
+    //     if (n % i == 0) {
+    //         printf("%d\n", i);
+    //     }
+    // }
+
+    // Q) Check if a number is a palindrome (e.g., 121 → 121).
+
+    // int n = 1222221, rem, rev = 0;
+    // int o = n;
+
+    // while(n != 0) {
+    //     rem = n % 10;
+    //     rev = rev * 10 + rem;
+    //     n /= 10;
+    // }
+
+    // if(o == rev) {
+    //     printf("The given number is palindrome.");
+    // }
+    // else {
+    //     printf("The given number is not a palindrome.");
+    // }
+
+    // Q) Find the GCD of two numbers using a loop.
+
+    // int num1 = 8, num2 = 12;
+    // int num1d, num2d, gcd;
+    
+    // for(int i = 1; i <= num1 && i <= num2; i++) {
+    //     if(num1 % i == 0 && num2 % i == 0) {
+    //         gcd = i;
+    //     }
+    // }
+
+    // printf("Gcd of %d and %d is: %d", num1, num2, gcd);
+
+    // Q) Print all prime numbers between 1 to N.
+
+    // int n = 20;
+
+    // for(int i = 1; i <= n; i++) {
+    //     int isPrime = 1;
+
+    //     for(int j = 2; j <= sqrt(i); j++) {
+    //         if (i % j == 0) {
+    //             isPrime = 0;
+    //             break;
+    //         }
+    //     }
+    //     if(isPrime) {
+    //         printf("%d ", i);
+    //     }
+    // }
+
+    // Q) Write a program to print a pattern like this:
+        //     *
+        //    * *
+        //   * * *
+        //  * * * *
+
+    // for(int i = 1; i <= 4; i++) {
+    //     for(int k = 4; k >= i - 1; k--) {
+    //         printf(" ");
+    //     }
+    //     for(int j = 1; j <= i; j++){
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
 
 
     return 0;
