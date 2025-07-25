732421c3d55d55d89e90ea19577c23469d7c9402
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-07-25 17:21:35 +0530

    Starting with pointers

---
+++ pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    // BASICS
+    
+    // Q) Declare an int variable and a pointer to it. Print:
+
+        // the variable value
+
+        // its address
+
+        // value using the pointer
+        // (Hint: Use & and * operators)
+    
+    
+
+    return 0;
+}
