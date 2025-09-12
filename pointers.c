23c09d3b8856eff56b45b979ecf4e8c890c11f80
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-09-12 06:07:16 +0530

    Pointters

--- pointers.c
+++ pointers.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+// void swap(int *a, int *b) {
+//     int temp;
+
+//     temp = *a;
+//     *a =  *b;
+//     *b = temp;
+
+// }
+
+// void incrementAll(int *p, int size) {
+//     for(int i = 0; i < size; i++) {
+//         p[i] += 1;
+//         printf("%d", p[i]);
+//     }
+// }
+
+// void reverse(int *a, int n) {
+//     for(int i = n - 1; i >= 0; i--) {
+//         printf("%d", *(a + i));
+//     }
+// }
+
 int main() {
 
     // BASICS
@@ -13,7 +35,110 @@ int main() {
         // value using the pointer
         // (Hint: Use & and * operators)
     
+    // int age = 21;
+    // int *pAge = NULL;
+
+    // pAge = &age;
+
+    // printf("Value: %d\n", age);
+    // printf("Value: %p\n", &age);
+    // printf("Value: %d\n", *pAge);
+
+    // Basic Pointer Declaration and Access
+    //   Task:
+    //         Declare an int variable x and assign it a value (e.g., 10).
+
+    // int x = 10;
+
+    //         Declare a pointer p to int and assign it the address of x.
+
+    // int *p = &x;
+
+    //         Print the value of x using the pointer p.
+
+    // printf("%d", *p);
+
+    //         Also print the address stored in p.
+
+    // printf("%p", p);
+
+
+    //  Swap Two Numbers Using Pointers
+
+    // int a, b;
+
+    // a = 10;
+    // b = 20;
+
+    // printf("a = %d\nb = %d\n", a, b);
+    
+    // swap(&a, &b);
+    
+    // printf("After swapping: \n");
+    // printf("a = %d\nb = %d", a, b);
+
+    // int arr[] = {1, 2, 3};
+
+    // incrementAll(arr, 3);
+
+    // int n;
+
+    // printf("\nEnter the number of elements: ");
+    // scanf("%d", &n);
+
+    // int arr[n];
+
+    // printf("\nEnter %d elements: ", n);
+    // for(int i = 0; i < n; i++) {
+    //     scanf("%d", &arr[i]);
+    // }
+
+    // reverse(arr, n);
+
+    // Write a C program that:
+
+    //     Declares an int variable (say a = 10).
+
+    //     Declares a pointer to that variable.
+
+    //     Prints the value of a two ways:
+
+    //     Directly with a.
+
+    //     Indirectly with the pointer (using *p).
+
+    // int a = 10;
+    // int *p = &a;
+
+    // printf("%d\n", a);
+    // printf("%d", *p);
+
+    // int a = 25;
+    // int *p = &a;
+
+    // printf("%p\n", &a);
+    // printf("%p\n", p);
+    // printf("%d", *p);
+
     
 
     return 0;
 }
+
+// #include <stdio.h>
+
+// void changeValue(int *p) {
+//     *p = *p + 10;
+// }
+
+// int main() {
+//     int x = 5;
+
+//     // Try changing this call:
+//     // changeValue(&x); // Option 1
+//     // changeValue(x); // Option 2
+
+//     printf("%d\n", x);
+//     return 0;
+// }
+
---
+++ recursion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// void disp(int n) {
+//     if(n == 0) return;
+
+//     disp(n - 1);
+//     printf("%d ", n);
+// }
+
+// int fact(int n) {
+//     if(n > 1) {
+//         return n * fact(n - 1);
+//     }
+//     else {
+//         return 1;
+//     }
+// }
+
+// int sum(int n) {
+//     if(n > 0) {
+//         return n + sum(n - 1);
+//     }
+// }
+
+int main() {
+
+    // BASICS
+
+    // Q) Print Numbers from 1 to N using Recursion
+        // (Example: Input: 5 → Output: 1 2 3 4 5)
+
+    // disp(5);
+    
+    // Q) Find Factorial using Recursion
+
+    // printf("%d ", fact(5));
+
+    // Q) Find the Sum of First N Natural Numbers
+
+        // (Input: 4 → Output: 10)
+    
+    // printf("%d", sum(4));
+
+    // MEDIUM
+
+    // Q) Check if a String is Palindrome (Recursively)
+        // (Input: "madam" → Output: Yes)
+
+    
+
+    return 0;
+}
